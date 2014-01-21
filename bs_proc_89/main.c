0e7dddf34c3de636f30d315c44178dfa0e9da69f
Markus Klemm <markus@markusklemm.net>
2014-01-21 11:49:56 +0100

    Initial Commit

---
+++ bs_proc_89/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  bs_proc_89
+//
+//  Created by Markus Klemm on 21.01.14.
+//  Copyright (c) 2014 net.markusklemm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
