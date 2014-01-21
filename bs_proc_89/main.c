1a61d0c99fadb905487f1aee38752c2fac246d0f
Markus Klemm <markus@markusklemm.net>
2014-01-22 00:15:39 +0100

    Aufgabe 2

--- bs_proc_89/main.c
+++ bs_proc_89/main.c
@@ -7,12 +7,43 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    srandom(time(NULL));
+    int processCount = atoi(argv[1]);
+    
+    int i;
+    for (i=0; i < processCount; i++) {
+        int forkreturn = fork();
+        if (forkreturn == -1) {
+            perror(NULL);
+            exit(EXIT_FAILURE);
+        }
+        else if (forkreturn != 0)
+        {
+            //Parent process
+            continue;
+        }
+        else
+        {
+            //Child
+            printf("I'm a child!\n");
+            sleep(10);
+            printf("Child is sleepy\n");
+            exit(EXIT_SUCCESS);
+        }
+        
+        
+    }
+    
+    sleep(20);
+    printf("Parent is sleepy\n");
+    
+    return EXIT_SUCCESS;
 }
 
